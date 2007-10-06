e42c9d300202983d6a12672a66d20c96bcbfd309
Jonny Schulz <jschulz@bloonix.de>
2007-10-06 02:59:34 +0000

    Import of BLOONIX/Sys-Statistics-Linux-0.18 from CPAN.
    gitpan-cpan-distribution: Sys-Statistics-Linux
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         BLOONIX/Sys-Statistics-Linux-0.18.tar.gz
    gitpan-cpan-author:       BLOONIX
    gitpan-cpan-maturity:     released

--- lib/Sys/Statistics/Linux/DiskUsage.pm
+++ lib/Sys/Statistics/Linux/DiskUsage.pm
@@ -64,7 +64,7 @@ This program is free software; you can redistribute it and/or modify it under th
 =cut
 
 package Sys::Statistics::Linux::DiskUsage;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use strict;
 use warnings;
