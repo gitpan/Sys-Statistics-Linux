17f76dd79f90606f0d18e9c39ac302f276cc27ce
Jonny Schulz <jschulz@bloonix.de>
2007-10-08 12:17:52 +0000

    Import of BLOONIX/Sys-Statistics-Linux-0.20 from CPAN.
    gitpan-cpan-distribution: Sys-Statistics-Linux
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         BLOONIX/Sys-Statistics-Linux-0.20.tar.gz
    gitpan-cpan-author:       BLOONIX
    gitpan-cpan-maturity:     released

--- lib/Sys/Statistics/Linux.pm
+++ lib/Sys/Statistics/Linux.pm
@@ -399,7 +399,7 @@ This program is free software; you can redistribute it and/or modify it under th
 =cut
 
 package Sys::Statistics::Linux;
-our $VERSION = '0.19';
+our $VERSION = '0.20';
 
 use strict;
 use warnings;
